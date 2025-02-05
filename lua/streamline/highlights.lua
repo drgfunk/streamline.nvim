215c4a1fec0e5ab250ea5e49dd2820fcce8c0668
Joshua Roof <josh.roof@gmail.com>
2025-02-05 18:38:59 -0500

    add highlight group for filetype, updates default hl groups

--- lua/streamline/highlights.lua
+++ lua/streamline/highlights.lua
@@ -3,6 +3,10 @@ local M = {}
 function M.setup()
 	-- Define your highlight groups here
 	vim.api.nvim_set_hl(0, "StreamlineMode", { fg = "#ff0000", bg = "#000000" })
+	vim.api.nvim_set_hl(0, "StreamlineGitBranch", { fg = "#ff0000", bg = "#000000" })
+	vim.api.nvim_set_hl(0, "StreamlineFilename", { fg = "#ff0000", bg = "#000000" })
+	vim.api.nvim_set_hl(0, "StreamlineModified", { fg = "#ff0000", bg = "#000000" })
+	vim.api.nvim_set_hl(0, "StreamlineFiletype", { fg = "#ff0000", bg = "#000000" })
 
 	-- import rose-pine colors and if found, use them
 	local rose_pine = require("rose-pine.palette")
@@ -11,6 +15,7 @@ function M.setup()
 		vim.api.nvim_set_hl(0, "StreamlineGitBranch", { fg = rose_pine.text, bg = rose_pine.overlay })
 		vim.api.nvim_set_hl(0, "StreamlineFilename", { fg = rose_pine.muted, bg = rose_pine.transparent })
 		vim.api.nvim_set_hl(0, "StreamlineModified", { fg = rose_pine.gold, bg = rose_pine.transparent })
+		vim.api.nvim_set_hl(0, "StreamlineFiletype", { fg = rose_pine.base, bg = rose_pine.love })
 	end
 end
 
